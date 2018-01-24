75c58bb19fa08fc4803f8f5824f00a975a450f95
sawasawa0 <sawasawaworks@gmail.com>
2018-01-24 16:40:54 +0900

    ogp

---
+++ sawasawaworks/js/ogp.js
@@ -0,0 +1,20 @@
+function img_find() {
+    var imgs = document.getElementsByTagName("img");
+    var imgSrcs = [];
+    for (var i = 0; i < imgs.length; i++) {
+        imgSrcs.push(imgs[i].src);
+    }
+    return imgSrcs;
+}
+
+var img = img_find();
+var ogmetatag = document.createElement('meta');
+
+ogmetatag.setAttribute('property', 'og:image');
+ogmetatag.setAttribute('content', img[0]);
+document.head.appendChild(ogmetatag);
+
+var twimgtag = document.createElement('meta');
+twimgtag.setAttribute('name', 'twitter:image:src');
+twimgtag.setAttribute('content', img[0]);
+document.head.appendChild(twimgtag);
